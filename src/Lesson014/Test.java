f68ae699194a374800d61c6870bdbafcc3571100
Artem Bogdanov <bogdanov-a-s2014@yandex.ru>
2021-04-14 21:28:26 +0300

    Added Lessons013-014

---
+++ src/Lesson014/Test.java
@@ -0,0 +1,43 @@
+package Lesson014;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class Test {
+    public static void main(String[] args) {
+        Person person1 = new Person(1);
+        Person person2 = new Person(2);
+        Person person3 = new Person(3);
+        Person person4 = new Person(4);
+
+//        Queue<Person> people = new LinkedList<>();
+        Queue<Person> people = new ArrayBlockingQueue<>(3);
+        people.add(person3);
+        people.add(person2);
+        people.add(person4);
+        people.add(person1);
+
+//        System.out.println(people.remove());
+//        System.out.println(people.peek());
+//        System.out.println(people);
+//
+//        for (Person person : people)
+//            System.out.println(person);
+    }
+}
+
+class Person {
+    private int id;
+
+    public Person(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                '}';
+    }
+}
