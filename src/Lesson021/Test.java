b972d8188df8aa1f015d9e189a82eb8e91ed27ec
Artem Bogdanov <bogdanov-a-s2014@yandex.ru>
2021-04-20 20:30:23 +0300

    Added Lessons021-023

---
+++ src/Lesson021/Test.java
@@ -0,0 +1,38 @@
+package Lesson021;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class Test {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        for(int i = 0; i < 5; i++)
+            executorService.submit(new Work(i));
+
+            executorService.shutdown();
+            System.out.println("All tasks submitted");
+
+            executorService.awaitTermination(1, TimeUnit.DAYS);
+    }
+}
+
+class Work implements Runnable {
+    private final int id;
+
+    public Work(int id){
+        this.id = id;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Work " + id + " was completed");
+    }
+}
