2b13532c8332a60fa24991e079a70fc0ed200799
Artem Bogdanov <bogdanov-a-s2014@yandex.ru>
2021-05-01 21:15:25 +0300

    Added Lessons038-039

---
+++ src/Lesson037-039/src/test/java/NetworkUtilsTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+public class NetworkUtilsTest {
+
+    @Test(timeout = 1000)
+    public void getConnectionShouldReturnFasterThanOneSecond() {
+//        try {
+//            Thread.sleep(100);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        NetworkUtils.getConnection();
+    }
+}
---
+++ src/Lesson037-039/src/test/java/Vector2DTest.java
@@ -0,0 +1,47 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class Vector2DTest {
+    private final double EPS = 1e-9;
+    private Vector2D v1;
+
+    @BeforeClass
+    public static void createNewVector() {
+        Vector2D v1 = new Vector2D();
+    }
+
+//    @Before
+//    public void createNewVector() {
+//        Vector2D v1 = new Vector2D();
+//    }
+
+    @Test
+    public void newVectorShouldHaveZeroLength() {
+        //action
+//        Vector2D v1 = new Vector2D();
+        //assertion
+        Assert.assertEquals(0, v1.length(), 1e-9);
+    }
+
+//    @Test
+//    public void newVectorShouldHaveZeroX() {
+
+//        Vector2D v1 = new Vector2D();
+//        Assert.assertEquals(0, v1.getX(), 1e-9);
+//    }
+
+//    @Test
+//    public void newVectorShouldHaveZeroY() {
+
+//        Vector2D v1 = new Vector2D();
+//        Assert.assertEquals(0, v1.getY(), 1e-9);
+//    }
+
+    @Test
+    public void newVectorShouldHaveZeroXAndAeroY() {
+        Assert.assertEquals(0, v1.getX(), 1e-9);
+        Assert.assertEquals(0, v1.getY(), 1e-9);
+    }
+}
